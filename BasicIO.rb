b2a1119964b85a51315e7b985e5b4c60af0cb58b
Alwaleed Algheraibi <alwaleedalgheraibi@Alwaleeds-MacBook-Pro.local>
2022-09-06 10:48:30 -0400

    added a new file named BasicIO.rb

---
+++ BasicIO.rb
@@ -0,0 +1,10 @@
+# Ruby provides a whole set of I/O related methods in the Kernel module
+# a Ruby module is like a java interface.
+
+# The print method prints each parameter its passed to standard out (stdout).
+# It does not append a new line to the end of its output.
+print "cat", [1,2,3], 99, "\n"
+print "cat", [1,2,3], 99, "\n"
+
+# Obeservations: The arguments to the print method weren't enclosed in parenthesis.
+# Delimiters like the semi-colon weren't, needed at the end of the lines
